d7fe3b0b705a7b80e5bdb43133e7959914b3876d
nezam0003 <fhameedk.nezam@gmail.com>
2021-02-14 22:46:25 +0600

    fetch data from text file using promise

--- lecture-1/demo.js
+++ lecture-1/demo.js
@@ -1,16 +1,20 @@
 const getButton = document.getElementById("btn");
+const showData = document.querySelector(".showData");
 const makeRequest = (e) => {
     e.preventDefault();
     console.log("button clicked");
-    const promiseObj = fetch("demo.txt");
-    console.log(promiseObj);
-    promiseObj
+    fetch("demo.txt")
         .then((res) => {
+            if (!res.ok) {
+                throw Error(res.statusText);
+            }
             console.log(res);
             return res.text();
         })
         .then((data) => {
+            showData.innerText = data;
             console.log(data);
-        });
+        })
+        .catch((error) => console.log(error));
 };
 getButton.addEventListener("click", makeRequest);
