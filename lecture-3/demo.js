16ccef8f99bc7559e1a31f50bd89bda0138437dd
nezam0003 <fhameedk.nezam@gmail.com>
2021-02-17 00:16:40 +0600

    fetch API using Async Await

--- lecture-3/demo.js
+++ lecture-3/demo.js
@@ -18,25 +18,46 @@ const getButton = document.getElementById("btn");
 // };
 
 // Fetch multiple data from API using promise
+// const allPost = document.getElementById("allpost");
+// const makeRequest = (e) => {
+//     console.log("button clicked");
+//     fetch("https://jsonplaceholder.typicode.com/posts")
+//         .then((res) => {
+//             console.log(res);
+//             if (!res.ok) {
+//                 throw Error(res.statusText);
+//             }
+//             return res.json();
+//         })
+//         .then((data) => {
+//             data.forEach((item) => {
+//                 allPost.innerHTML += `
+//                <div> ID :${item.id}</div>
+//                <div> Title :${item.title}</div>
+//                `;
+//             });
+//         })
+//         .catch((error) => console.log(error));
+// };
+
+//  using Async Await
 const allPost = document.getElementById("allpost");
-const makeRequest = (e) => {
-    console.log("button clicked");
-    fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((res) => {
-            console.log(res);
-            if (!res.ok) {
-                throw Error(res.statusText);
-            }
-            return res.json();
-        })
-        .then((data) => {
-            data.forEach((item) => {
-                allPost.innerHTML += `
-               <div> ID :${item.id}</div>
-               <div> Title :${item.title}</div>
+const makeRequest = async() => {
+    try {
+        console.log("clicked");
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        if (!res.ok) {
+            throw Error(res.statusText);
+        }
+        const showData = await res.json();
+        showData.forEach((data) => {
+            allPost.innerHTML += `
+               <div> ID :${data.id}</div>
+               <div> Title :${data.title}</div>
                `;
-            });
-        })
-        .catch((error) => console.log(error));
+        });
+    } catch (error) {
+        console.log(error);
+    }
 };
 getButton.addEventListener("click", makeRequest);
